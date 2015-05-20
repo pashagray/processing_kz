fced30f9a0b7ba99997ce82e32d9aae7c8f747e5
Pavel Tkachenko <tpepost@gmail.com>
2015-05-20 13:41:34 +0600

    Template for config added

---
+++ lib/generators/processing_kz/config/templates/processing_kz.rb
@@ -0,0 +1,7 @@
+ProcessingKz.config do |config|
+  config.wsdl = 'wsdl address'
+  config.host = 'processing_kz server address'
+  config.merchant_id = 'your merhant id'
+  config.language_code = 'ru' # ru, en or kz
+  config.currency_code = 398 # KZT code
+end
